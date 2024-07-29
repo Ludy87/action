558afb5cd5e974834f3bb4d63987cb3945a3bf88
Ludy87 <Ludy87@users.noreply.github.com>
2024-07-29 17:52:40 +0200

    Update index.js

--- pr-issues-title-check/dist/index.js
+++ pr-issues-title-check/dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 7351:
@@ -31322,4 +31322,3 @@ module.exports = parseParams
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
