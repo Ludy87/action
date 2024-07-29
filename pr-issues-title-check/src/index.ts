f071f9a1d60e877c444d68105f360302e1ec7542
Ludy87 <Ludy87@users.noreply.github.com>
2024-07-29 20:04:28 +0200

    Update index.ts

--- pr-issues-title-check/src/index.ts
+++ pr-issues-title-check/src/index.ts
@@ -34,11 +34,14 @@ async function run(): Promise<void> {
         );
         const actor = github.context.actor;
 
+        core.info(`${actor} actor`);
+
         actorWithoutRestriction.forEach((a) => {
             if (a === actor) {
                 core.info(`${actor} has no limitation`);
                 return;
             }
+            core.info(a);
         });
 
         core.info(`minLen: ${issuesMinLen}`);
